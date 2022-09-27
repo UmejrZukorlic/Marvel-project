45ea9713b5f31af74808b3df659561dfc6ccf6b9
Umejr Zukorlic <89653651+UmejrZukorlic@users.noreply.github.com>
2022-09-27 17:15:03 +0200

    feath adding header

---
+++ src/components/Layout/layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Header from "./Header/header";
+
+const Layout = (props) => {
+  return (
+    <>
+      <Header />
+      {props.children}
+    </>
+  );
+};
+
+export default Layout;
