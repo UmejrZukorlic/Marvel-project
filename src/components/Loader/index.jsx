c9b88cbd8ede52105c3a9999be9245b06c380114
Umejr Zukorlic <89653651+UmejrZukorlic@users.noreply.github.com>
2023-01-12 21:50:41 +0100

    add new loader

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "../HomePage/homePage.css";
 
 const Loading = () => {
-  return <div></div>;
+  return (
+    <div className="loader">
+      <div className="lds-ellipsis">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
